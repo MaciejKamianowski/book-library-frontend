e9d66aaf883c6b70375583e51c05eeb0d67c2d5e
Maciej Kamianowski <kamianowskimaciej@gmail.com>
2022-12-08 21:55:56 +0100

    created basic website lookup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { ListBooksComponent } from './list-books/list-books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', component : ListBooksComponent},
+
+  { path : '**', component : ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
